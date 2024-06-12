355fdefb3cf482d3cc51d458cad48d6cc568133f
romeoaditya <106458722+romeoaditya@users.noreply.github.com>
2024-06-12 11:10:02 +0700

    Update Data.jsx

--- src/components/testimonials/Data.jsx
+++ src/components/testimonials/Data.jsx
@@ -1,28 +1,28 @@
 import React from "react";
-import Image1 from "../../assets/testimonial1.png";
-import Image2 from "../../assets/testimonial2.png";
-import Image3 from "../../assets/testimonial3.png";
+import Image1 from "../../assets/ez.png";
+import Image2 from "../../assets/secret.png";
+import Image3 from "../../assets/z.jpg";
 
 export const Data = [
   {
     id: 1,
     image: Image1,
-    title: "Jhon Doe",
+    title: "EZ - Computer",
     description:
       "A really good job, all aspects of the project were followed step by step and with good results.",
   },
   {
     id: 2,
     image: Image2,
-    title: "Harry Clinton",
+    title: "Secret Company ID",
     description:
-      "A really good job, all aspects of the project were followed step by step and with good results.",
+      "Awesome, all parts were tested properly and correctly, with a report that is very easy to understand.",
   },
   {
     id: 3,
     image: Image3,
-    title: "Sara Cill",
+    title: "Zara D - ID",
     description:
-      "A really good job, all aspects of the project were followed step by step and with good results.",
+      "The frontend design is very good, and interesting to look at.",
   },
 ];
