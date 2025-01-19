d7c3bc01bc4d51e2ddc987e31901f3e8cdf60249
romeoaditya <106458722+romeoaditya@users.noreply.github.com>
2025-01-19 15:13:43 +0700

    Update WorksItems.jsx

--- src/components/work/WorksItems.jsx
+++ src/components/work/WorksItems.jsx
@@ -5,7 +5,7 @@ const WorksItems = ({item}) => {
     <div className="work__card" key={item.id}>
       <img src={item.image} className="work__img" alt="" />
       <h3 className="work__title">{item.title}</h3>
-      <a href="#" className="work__button">
+      <a href="https://github.com/romeoaditya/booking-tiket-wisata.git" className="work__button">
         Demo <i className="bx bx-right-arrow-alt work__button-icon"></i>
       </a>
     </div>
