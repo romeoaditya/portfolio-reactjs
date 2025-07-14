4c5bd2c71f20b16f6a1c30614b8fc3e1566f7889
romeoaditya <romeoaditya22@gmail.com>
2025-07-14 20:52:27 +0700

    update cv

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./about.css";
 import AboutImg from "../../assets/ABOUT2.jpg";
-import CV from "../../assets/John-Cv.pdf";
+import CV from "../../assets/RomeoAditya-CV.pdf";
 import Info from "./Info";
 
 const About = () => {
